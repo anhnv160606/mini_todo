596588cfd841512cc5ea59022a421450d88557d1
nguyen van anh <anhnguyenvan161616@gmail.com>
2025-05-28 23:13:24 +0700

    add cac thu muc goc

---
+++ cli.py
@@ -0,0 +1,13 @@
+import argparse
+from todo import TodoList
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Todo List Manager")
+    # Thêm các argument tương ứng với các tính năng
+    args = parser.parse_args()
+    # Xử lý các lệnh
+
+
+if __name__ == "__main__":
+    main()
